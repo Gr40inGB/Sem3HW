11da6bd38b17c6f2c05f7432ed424a8a8635295a
Rishat_Imangulov <gr40in@yandex.ru>
2023-01-22 12:59:36 +0500

    start task2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,19 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.Clear();
+
+Console.Write("Введите число: ");
+int inputNumber = int.Parse(Console.ReadLine());  // введенное число 
+int inputNumberToCut = Math.Abs(inputNumber);  // это число будем отрезать по десятку справа / 
+int invertNumber = 0;               // это число которое будет наращиваться из вырезанного   - и в итоге получим перевернутое ввёденное число
+
+while (inputNumberToCut % 10 > 0) //  пока в вырезаемом числе есть хоть какие-то числа перед зяпятой
+{
+    invertNumber = invertNumber * 10 + inputNumberToCut % 10;   // по сути двигаем число умножая на 10,  освобождая место для прибавления новой цифры
+    inputNumberToCut = Convert.ToInt32(inputNumberToCut) / 10;  // отрезаем число, так как последнее ууже пибавили в инвертируемому 
+}
+
+if (invertNumber == Math.Abs(inputNumber))  // сравниваем полученные числа
+    System.Console.WriteLine($"Вы ввели число {inputNumber} - это полиндром ");
+else System.Console.WriteLine($"Вы ввели число {inputNumber} - это НЕ полиндром ");
