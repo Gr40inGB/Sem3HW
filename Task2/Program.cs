156f2bcf603a3b40704849c28ff06237b0cf5186
Rishat_Imangulov <gr40in@yandex.ru>
2023-01-22 14:58:12 +0500

    end task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,5 +6,45 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-//Расстояние = $ \ sqrt {(x2-x1) ^{2} + (y2-y1) ^{2} + (z2-z1) ^{2}} $ 
+//Расстояние = $ \ sqrt {(x2-x1) ^{2} + (y2-y1) ^{2} + (z2-z1) ^{2}} $ +
 
+Console.Clear();
+
+double x1, y1, z1, x2, y2, z2 = 0;
+
+System.Console.Write("Введите координату X1: ");
+x1 = Convert.ToSingle(Console.ReadLine());
+System.Console.Write("Введите координату Y1: ");
+y1 = Convert.ToSingle(Console.ReadLine());
+System.Console.Write("Введите координату Z1: ");
+z1 = Convert.ToSingle(Console.ReadLine());
+System.Console.Write("\nВведите координату X2: ");
+x2 = Convert.ToSingle(Console.ReadLine());
+System.Console.Write("Введите координату Y2: ");
+y2 = Convert.ToSingle(Console.ReadLine());
+System.Console.Write("Введите координату Z2: ");
+z2 = Convert.ToSingle(Console.ReadLine());
+
+Console.Clear();
+System.Console.WriteLine("Вы ввели координаты:");
+System.Console.SetCursorPosition(4, 1);
+System.Console.WriteLine($"X1: {x1}");
+System.Console.SetCursorPosition(20, 1);
+System.Console.WriteLine($"Y1: {y1}");
+System.Console.SetCursorPosition(36, 1);
+System.Console.WriteLine($"Z1: {z1}");
+System.Console.SetCursorPosition(4, 4);
+
+System.Console.SetCursorPosition(4, 2);
+System.Console.WriteLine($"X2: {x2}");
+System.Console.SetCursorPosition(20, 2);
+System.Console.WriteLine($"Y2: {y2}");
+System.Console.SetCursorPosition(36, 2);
+System.Console.WriteLine($"Z2: {z2}");
+System.Console.SetCursorPosition(0, 4);
+
+double distance = Math.Pow(
+                    Math.Pow(x1 - x2, 2) +
+                    Math.Pow(y1 - y2, 2) +
+                    Math.Pow(z1 - z2, 2), 0.5);
+System.Console.WriteLine($"Расстояние между точками: {Math.Round(distance, 2, MidpointRounding.ToZero)}");
